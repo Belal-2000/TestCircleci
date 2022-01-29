11ba078dbd27bcc2dd8a69533a6f5fe5540d56e5
belal011 <belalma3arek@gmail.com>
2022-01-29 03:28:22 +0200

    cd

--- reactnd-contacts-complete/src/utils/ContactsAPI.js
+++ reactnd-contacts-complete/src/utils/ContactsAPI.js
@@ -1,4 +1,4 @@
-const api = process.env.REACT_APP_CONTACTS_API_URL || 'http://reactnd-env.eba-9werppqp.us-east-2.elasticbeanstalk.com'
+const api = process.env.REACT_APP_CONTACTS_API_URL || 'http://elasticapptest2-env.eba-h2c8bnec.us-east-1.elasticbeanstalk.com'
 
 let token = localStorage.token
 
